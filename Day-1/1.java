0195c379d8b571ebeee8b9ae157bda5b8da03b44
anshika52004 <agarwalanshika74@gmail.com>
2025-07-30 11:19:45 +0530

    add

--- Day-1/1.java
+++ Day-1/1.java
@@ -1,12 +1,13 @@
 import java.util.*;
 
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
-    
-    while (a<5){
-        a+=2;
-        System.out.println(a);
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        
+        while (a<5){
+            a+=2;
+            System.out.println(a);
+        }
     }
-    
 }
